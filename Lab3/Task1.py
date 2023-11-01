d81ea2c8966046a42c829b40f395fc9f6361119a
Merkroc <merkroc@gmail.com>
2023-11-01 11:46:16 +0300

    Added Task1 of Lab3 and list of tasks

---
+++ Lab3/Task1.py
@@ -0,0 +1,17 @@
+with open("F1.txt", 'w') as f1:
+    new_str = '1'
+    while new_str != '':
+        new_str = input()
+        f1.write(new_str+'\n')
+
+with open("F2.txt", 'w') as f2:
+    f1 = open("F1.txt")
+    str_f1 = f1.readlines()
+    for i in str_f1:
+        alpha = True
+        for k in i:
+            if k.isdigit():
+                alpha = False
+                break
+        if alpha:
+            f2.write(i)
